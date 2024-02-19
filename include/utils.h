3d2bdd2bec91d91303424256b676bed1a8bdaa15
Thibault GOUNANT <thibault.gounant@etu.univ-nantes.fr>
2024-02-19 20:46:56 +0100

    Add is_sorted

--- include/utils.h
+++ include/utils.h
@@ -2,5 +2,6 @@
 #define SORTING_ALGORITHMS_UTILS_H
 
 void swap(int *a, int *b);
+bool is_sorted(const int array[], int size);
 
 #endif
--- src/utils.c
+++ src/utils.c
@@ -4,3 +4,11 @@ void swap(int *a, int *b) {
     *a = *b;
     *b = temp;
 }
+
+bool is_sorted(const int array[], int size) {
+    bool sorted = true;
+    for (int i = 0; i < size - 1 && sorted; ++i) {
+        sorted = array[i] <= array[i+1];
+    }
+    return sorted;
+}
