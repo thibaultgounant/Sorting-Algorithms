0cbf52e65833ac9e68accd432d6e272be0f34c6d
Thibault GOUNANT <thibault.gounant@etu.univ-nantes.fr>
2024-03-06 23:41:50 +0100

    Open window

--- src/main.c
+++ src/main.c
@@ -6,7 +6,50 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#define WIDTH 1280
+#define HEIGHT 720
+
 int main(int argc, char *argv[]) {
-    printf("Hello, World!\n");
+
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        return EXIT_FAILURE;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Sorting Visualization", NULL, NULL);
+
+    if (!window) {
+        fprintf(stderr, "Failed to create GLFW window\n");
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwSwapInterval(1);
+
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
     return EXIT_SUCCESS;
 }
